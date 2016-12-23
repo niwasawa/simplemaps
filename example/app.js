87165e916f16ede213498cd6243ff54bf43c75e3
Naoki Iwasawa <niwasawa@maigo.info>
2016-12-23 15:17:31 +0900

    merge appconfig.json to app.js

--- example/app.js
+++ example/app.js
@@ -2,14 +2,29 @@
 
 var http   = require('http');
 var url    = require('url');
-var config = require('./appconfig.json');
 var simplemaps = require('simplemaps');
+
+var config = {
+  "server_port": 8888,
+  "base_url": "/maps/",
+  "template_html": "template.html",
+  "appid": "<Your Application ID by Yahoo! JAPAN Developer Network>",
+  "mapspec": {
+    "width": 600,
+    "height": 600,
+    "min_z": 1,
+    "max_z": 20,
+    "default_lat": 35.170429,
+    "default_lon": 136.882298,
+    "default_z": 6
+  }
+};
 
 var server = http.createServer(function(req, res) {
 
   try{
     var pathname = url.parse(req.url).pathname;
-    if (simplemaps.Utils.forwardMatch(pathname, config.base_url)) { 
+    if (simplemaps.Utils.forwardMatch(pathname, config.base_url)) {
       new simplemaps.WebApp(config).invoke(req, res);
     } else {
       simplemaps.Utils.errorPage(req, res);
